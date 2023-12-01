4e6a62848607253fd308d0a2993a0b535f2df3b0
Anissa Molina <ani_luz15@yahoo.com>
2023-12-01 10:35:45 -0800

    almost done
    will finish over weekend

---
+++ app.js
@@ -0,0 +1,13 @@
+let overlayElem = document.getElementById('packageoverlay'),
+    closeOverlay = document.getElementById('overlay-close1'),
+    openOverlay = document.getElementsByClassName('button2');
+
+closeOverlay.addEventListener("click", overlayToggle);
+
+for(elem of openOverlay){
+    elem.addEventListener('click', overlayToggle);
+}
+
+function overlayToggle(){
+    overlayElem.classList.toggle('visible');
+}
