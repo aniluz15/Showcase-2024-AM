e079e350d74f3d36964c5ee1a9f7001aa09be683
Anissa Molina <ani_luz15@yahoo.com>
2023-12-08 09:00:46 -0800

    WE DONE
    for now , we think

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let overlayElem = document.getElementById('packageoverlay'),
     closeOverlay = document.getElementById('overlay-close1'),
     openOverlay = document.getElementsByClassName('button2');
+    openOverlay = document.getElementsByClassName('button3');
 
 closeOverlay.addEventListener("click", overlayToggle);
 
